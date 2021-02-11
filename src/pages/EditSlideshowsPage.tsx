3e96a92148ae81b0861ed54f791f71c6f5f2fc1a
ucabcnd <73539195+ucabcnd@users.noreply.github.com>
2021-02-11 15:19:13 +0000

    added music folder, fixed reorderable components

--- src/pages/EditSlideshowsPage.tsx
+++ src/pages/EditSlideshowsPage.tsx
@@ -1,8 +1,26 @@
 import { IonContent, IonHeader, IonItemDivider, IonPage, IonTitle, IonToolbar, IonList, IonReorderGroup, IonItem, IonLabel } from '@ionic/react';
 import Slideshow_Item from '../components/Slideshow_Item/Slideshow_Item';
+import { ItemReorderEventDetail } from '@ionic/core';
 
 let myitems = ['one', 'two', 'three', 'four', 'five'];
 /*<Slideshow_Item name={{ item }}*/
+
+function doReorder(event: CustomEvent<ItemReorderEventDetail>) {
+  // The `from` and `to` properties contain the index of the item
+  // when the drag started and ended, respectively
+  console.log(myitems)
+  console.log('Dragged from index', event.detail.from, 'to', event.detail.to);
+
+  let itemtomove = myitems.splice(event.detail.from, 1)[0];
+  myitems.splice(event.detail.to, 0, itemtomove);
+
+  console.log(myitems)
+
+  // Finish the reorder and position the item in the DOM based on
+  // where the gesture ended. This method can also be called directly
+  // by the reorder group
+  event.detail.complete();
+}
 
 const EditSlideshowsPage: React.FC = () => (
   < IonPage >
@@ -13,7 +31,7 @@ const EditSlideshowsPage: React.FC = () => (
         </IonHeader>
     <IonContent fullscreen>
       <IonList>
-        <IonReorderGroup disabled={false}>
+        <IonReorderGroup disabled={false} onIonItemReorder={doReorder}>
           {myitems.map(item => <Slideshow_Item name={ item }/>)}
         </IonReorderGroup>
       </IonList >
