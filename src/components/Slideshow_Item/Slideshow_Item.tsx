3c0129de4e82ed7b48f121f5ea9426c2ee0b4322
ucabcnd <73539195+ucabcnd@users.noreply.github.com>
2021-02-20 23:51:12 +0000

    made slideshow name a button

--- src/components/Slideshow_Item/Slideshow_Item.tsx
+++ src/components/Slideshow_Item/Slideshow_Item.tsx
@@ -1,8 +1,7 @@
 import './Slideshow_Item.css';
 import '@ionic/react'
-import { IonItem, IonLabel, IonReorder, IonList, IonReorderGroup, IonCheckbox, IonGrid, IonRow, IonCol } from '@ionic/react';
-import React, { useState, useEffect } from 'react';
-import { ItemReorderEventDetail } from '@ionic/core';
+import { IonItem, IonLabel, IonReorder, IonList, IonReorderGroup, IonCheckbox, IonGrid, IonRow, IonCol, IonButton } from '@ionic/react';
+import React from 'react';
 
 interface ContainerProps {
   name: string;
@@ -15,7 +14,7 @@ const Slideshow_Item: React.FC<ContainerProps> = ({ name, editing }) => (
       <IonGrid>
         <IonRow>
           <IonCol className='checkcontainer'>{editing ? <IonCheckbox checked={true} className='checkbox'/>: null}</IonCol>
-          <IonCol className='slidename'>{name}</IonCol>
+          <IonCol className='slidename'><IonButton fill='clear' routerLink='slideshowpage'>{name}</IonButton></IonCol>
           <IonCol className='reordergrabber'>{editing ? <IonReorder className='grabicon'/>: null}</IonCol>
         </IonRow>
       </IonGrid>
