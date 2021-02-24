faa9ec637f0df82a36bff6ca6d24c11bbb5cdf14
Caroline Crandell <carolineecrandell@gmail.com>
2021-02-24 11:26:22 -0800

    Support modals

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,14 +1,18 @@
 import { IonRange, IonIcon } from "@ionic/react";
-// import { sunny } from "ionicons/icons";
+import React, { useState } from "react";
+import { sunny } from "ionicons/icons";
 import "./Slider.css";
 
-const Slider: React.FC = () => (
-  <div>
-    <IonRange>
-      {/* <IonIcon size="small" slot="start" icon={sunny} />
-      <IonIcon slot="end" icon={sunny} /> */}
-    </IonRange>
-  </div>
-);
+const Slider: React.FC = () => {
+  const [value, setValue] = useState(0);
+  return (
+    <div>
+      <IonRange>
+        <IonIcon size="small" slot="start" icon={sunny} />
+        <IonIcon slot="end" icon={sunny} />
+      </IonRange>
+    </div>
+  );
+};
 
 export default Slider;
