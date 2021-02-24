54a4b0dc68852622fd142390e2961cdf6bbadad6
ucabcnd <73539195+ucabcnd@users.noreply.github.com>
2021-02-24 23:27:35 +0000

    can now change name of slideshows
    Mwahahahahah

--- src/components/User_Input Slideshow/User_Input_Slideshow.tsx
+++ src/components/User_Input Slideshow/User_Input_Slideshow.tsx
@@ -2,25 +2,27 @@ import './User_Input_Slideshow.css';
 import '@ionic/react'
 import { IonItem, IonLabel, IonInput } from '@ionic/react';
 import React, { useRef } from 'react';
-import { InputChangeEventDetail } from '@ionic/core';
 import {Selected_Slideshow_Context} from '../../pages/Selected_Slideshow_Context';
 
 interface ContainerProps {
   name: string;
-  /*value: string;*/
 }
 
-/*(event: any) => setValue(event.target.value)*/
 
 const User_Input_Slideshow: React.FC<ContainerProps> = (props) => {
+  
+  const Slideshow_current_name = Selected_Slideshow_Context();
 
   let valueRef = useRef <HTMLIonInputElement>(null);
-  const Slideshow_current_name = Selected_Slideshow_Context();
+
+  function update_name(){
+    Slideshow_current_name.name = valueRef.current.value;
+  }
 
   return (
     <IonItem className='ionlabel'>
       <IonLabel position="stacked">{props.name}</IonLabel>
-      <IonInput id="input" ref={valueRef} placeholder='Enter name here' value={Slideshow_current_name.name}></IonInput>
+      <IonInput id="input" ref={valueRef} placeholder='Enter name here' value={Slideshow_current_name.name} onIonChange={update_name}/>
     </IonItem>
   );
 }
