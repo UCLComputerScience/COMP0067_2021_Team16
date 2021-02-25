e08d563f424747454b8b07c69be388b40f725ab8
ucabcnd <73539195+ucabcnd@users.noreply.github.com>
2021-02-25 14:05:19 +0000

    Can add new Slideshow to list

--- src/components/AddNewSlideshow/AddNewSlideshow.tsx
+++ src/components/AddNewSlideshow/AddNewSlideshow.tsx
@@ -1,13 +1,20 @@
 import {IonButton,IonIcon} from '@ionic/react';
 import {addCircleSharp} from 'ionicons/icons';
-import {Update_Selected_Slideshow_Context} from '../../contexts/Slideshow_Context';
+import {Update_Selected_Slideshow_Context,Access_Slideshow_List} from '../../contexts/Slideshow_Context';
 import Slideshow_Class from '../../classes/Slideshow_Class';
 
 const AddNewSlideshow: React.FC = () =>{
     const Update_Sel_Slideshow = Update_Selected_Slideshow_Context();
+    const Slideshow_List = Access_Slideshow_List();
+
+    function whenclicked(){
+        const new_Slideshow = new Slideshow_Class(null,null,true);
+        Update_Sel_Slideshow(new_Slideshow);
+        Slideshow_List.push(new_Slideshow);
+    }
     
     return(
-        <IonButton fill='clear' className='addbutton' routerLink='/EditSlidesPage' onClick={()=>Update_Sel_Slideshow(new Slideshow_Class(null,null,true))}>
+        <IonButton fill='clear' className='addbutton' routerLink='/EditSlidesPage' onClick={()=>whenclicked()}>
             <IonIcon icon={addCircleSharp} className='addicon'/>
         </IonButton>
     );
