859f322e49aa2e916f50eea3a25a86988b774d21
Giuseppe Baldini <giuseppebaldini@live.com>
2021-02-25 16:56:19 +0000

    Updated Record Audio page

--- src/pages/RecordAudio/RecordAudio.tsx
+++ src/pages/RecordAudio/RecordAudio.tsx
@@ -5,8 +5,8 @@ import {saveOutline, trashOutline} from 'ionicons/icons';
 const RecordAudio: React.FC = () => (
   <IonGrid className="ion-grid">
     <IonRow>
-      <IonCol className="ion-column"><IonButton fill="clear" className="ion-button" routerLink='/SubscribePage'><IonIcon icon={saveOutline} size="large" /></IonButton></IonCol>
-      <IonCol className="ion-column"><IonButton fill="clear" routerLink="/AudioSettings"><IonIcon icon={trashOutline} size="large" /></IonButton></IonCol>
+      <IonCol className="ion-column"><IonButton fill="clear" className="ion-button"><IonIcon icon={saveOutline} size="large" /></IonButton></IonCol>
+      <IonCol className="ion-column"><IonButton fill="clear" ><IonIcon icon={trashOutline} size="large" /></IonButton></IonCol>
     </IonRow>
   </IonGrid>
 );
