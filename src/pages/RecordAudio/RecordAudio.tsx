4e049af77d4be5a285eb45127e6a57b94e1b49f1
Giuseppe Baldini <giuseppebaldini@live.com>
2021-02-25 14:02:29 +0000

    Created Record Audio page

---
+++ src/pages/RecordAudio/RecordAudio.tsx
@@ -0,0 +1,8 @@
+import { IonButton, IonGrid, IonRow, IonCol, IonIcon} from '@ionic/react';
+import {micOutline, trashOutline} from 'ionicons/icons';
+
+
+const RecordAudio: React.FC = () => (
+);
+
+export default RecordAudio;
