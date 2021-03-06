fa6b6dcb78abd6a4174989324d39c4ff557c1409
Caroline Crandell <carolineecrandell@gmail.com>
2021-03-06 07:47:31 -0800

    Form submit

--- backend/app/public/js/baby.js
+++ backend/app/public/js/baby.js
@@ -51,45 +51,6 @@ $(document).ready(function () {
 
   })
 
-  $("#image-submit").on("click", function (event) {
-    event.preventDefault();
-
-    let newImage = {
-      type: "default",
-      name: $("#name").val().trim().toUpperCase(),
-      png: $("#name").val().trim().replace(/[^A-Z\d\s]/gi, '').replace(/\s/g, '').toLowerCase() + ".png"
-    };
-
-    console.log(newImage);
-
-    $.post("/images/new", newImage)
-      .then(function () {
-        $("#image-area").empty()
-        $.get("/images/all", function (data) {
-
-          if (data.length !== 0) {
-            let table = $("<table><tr><th>ID</th><th>Name</th><th>Narration</th><th>Image</th><th>Audio</th><th>Actions</th></tr>");
-            for (let i = 0; i < data.length; i++) {
-              let row = $("<tr>");
-              row.append("<td>" + data[i].id + "</td>");
-              row.append("<td>" + data[i].name + "</td>");
-              row.append("<td>" + data[i].text + "</td>");
-              row.append("<td><img class='tableimg' src='/images/" + data[i].png + "'</img></td>");
-              row.append("<td>" + data[i].default_mp3 + "</td>");
-              row.append("<td><button class='delete' value='" + data[i].id + "'>Delete</button></td>");
-              row.append("</tr>");
-              table.append(row);
-            }
-            table.append("</table>")
-            $("#image-area").prepend(table)
-          }
-
-        })
-      });
-
-    $("#name").val("");
-  });
-
   let fileTag = document.getElementById("filetag"),
     preview = document.getElementById("preview");
 
--- backend/app/routes/api-routes.js
+++ backend/app/routes/api-routes.js
@@ -1,40 +1,59 @@
+const fs = require('fs');
 const connection = require("../config/connection.js");
 
-module.exports = function(app) {
-  app.get("/images/all", function(req, res) {
+module.exports = function (app) {
+  app.get("/images/all", function (req, res) {
     let dbQuery = "SELECT * FROM images ORDER BY id ASC";
 
-    connection.query(dbQuery, function(err, result) {
+    connection.query(dbQuery, function (err, result) {
       if (err) throw err;
       res.json(result);
     });
   });
 
-  app.post("/images/new", function(req, res) {
+  app.post("/images/new", function (req, res) {
     console.log("Image Data:");
     console.log(req.body);
-    
-    let dbQuery = "INSERT INTO images (type, name, png) VALUES (?,?,?)";
+    let newImage = {
+          type: "default",
+          name: req.body.name.trim().toUpperCase(),
+          text: req.body.narration.trim().replace(/\.\s+([a-z])[^\.]|^(\s*[a-z])[^\.]/g, s => s.replace(/([a-z])/,s => s.toUpperCase())),
+          png: req.body.name.trim().replace(/[^A-Z\d\s]/gi, '').replace(/\s/g, '').toLowerCase() + ".png"
+        };
 
-    connection.query(dbQuery, [req.body.type, req.body.name, req.body.png], function(err, result) {
+    let dbQuery = "INSERT INTO images (type, name, text, png) VALUES (?,?,?,?)";
+
+    connection.query(dbQuery, [newImage.type, newImage.name, newImage.text, newImage.png], function (err, result) {
       if (err) throw err;
       console.log("Image successfully saved!");
-      res.end();
+      res.redirect('/views/addimage.html'); 
     });
   });
 
-  app.delete("/images/delete/:id", function(req, res) {
+  // app.post('/images/upload', function (req, res) {
+  //   fs.readFile(req.files.image.path, function (err, data) {
+  //     if (err) throw err;
+  //     let imageName = req.files.image.name
+  //     let newPath = __dirname + "/images/" + imageName;
+  //     fs.writeFile(newPath, data, function (err) {
+  //       if (err) throw err;
+  //       res.redirect("/images/" + imageName);
+  //     })
+  //   })
+  // })
+
+  app.delete("/images/delete/:id", function (req, res) {
     console.log(req.params);
 
     let dbQuery = "DELETE FROM images WHERE id = ?";
-    connection.query(dbQuery, [req.params.id], function(err, result) {
+    connection.query(dbQuery, [req.params.id], function (err, result) {
       if (err) throw err;
       console.log("Image deleted from images!");
       res.end();
     });
 
     let dbQuery2 = "DELETE FROM slideshows WHERE images_id = ?";
-    connection.query(dbQuery2, [req.params.id], function(err, result) {
+    connection.query(dbQuery2, [req.params.id], function (err, result) {
       if (err) throw err;
       console.log("Image deleted from slideshows!");
       res.end();
