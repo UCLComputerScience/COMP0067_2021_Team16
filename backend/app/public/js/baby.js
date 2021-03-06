1700f29780a73ca3a7b0c79df746ca58d780cf36
Caroline Crandell <carolineecrandell@gmail.com>
2021-03-06 04:22:38 -0800

    Mailing list

--- backend/app/public/js/baby.js
+++ backend/app/public/js/baby.js
@@ -1,4 +1,3 @@
-
 $.get("/images/all", function (data) {
 
   if (data.length !== 0) {
@@ -33,18 +32,49 @@ $("#image-submit").on("click", function (event) {
 
   $.post("/images/new", newImage)
     .then(function () {
-      let row = $("<div>");
-      row.append("<tr>");
-      row.append("<td>" + newImage.id + "</td>");
-      row.append("<td>" + newImage.name + "</td>");
-      row.append("<td>" + newImage.text + "</td>");
-      row.append("<td><img class='tableimg' src='/images/" + newImage.png + "'</img></td>");
-      row.append("<td>" + newImage.default_mp3 + "</td>");
-      row.append("<td><button id='" + newImage.id + "'>Edit</button><button id='" + newImage.id + "'>Delete</button></td>");
-      row.append("</tr>");
-      $("#image-area").prepend(row);
+      $("#image-area").empty()
+      $.get("/images/all", function (data) {
 
+        if (data.length !== 0) {
+          let table = $("<table><tr><th>ID</th><th>Name</th><th>Narration</th><th>Image</th><th>Audio</th><th>Actions</th></tr>");
+          for (let i = 0; i < data.length; i++) {
+            let row = $("<tr>");
+            row.append("<td>" + data[i].id + "</td>");
+            row.append("<td>" + data[i].name + "</td>");
+            row.append("<td>" + data[i].text + "</td>");
+            row.append("<td><img class='tableimg' src='/images/" + data[i].png + "'</img></td>");
+            row.append("<td>" + data[i].default_mp3 + "</td>");
+            row.append("<td><button id='" + data[i].id + "'>Edit</button><button id='" + data[i].id + "'>Delete</button></td>");
+            row.append("</tr>");
+            table.append(row);
+          }
+          table.append("</table>")
+          $("#image-area").prepend(table)
+        }
+
+      })
     });
 
   $("#name").val("");
 });
+
+let fileTag = document.getElementById("filetag"),
+  preview = document.getElementById("preview");
+
+fileTag.addEventListener("change", function () {
+  changeImage(this);
+});
+
+function changeImage(input) {
+  let reader;
+
+  if (input.files && input.files[0]) {
+    reader = new FileReader();
+
+    reader.onload = function (e) {
+      preview.setAttribute('src', e.target.result);
+    }
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
