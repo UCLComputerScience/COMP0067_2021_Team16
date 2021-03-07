582366eecbdb082933e02a9b3dd069acaa524353
Caroline Crandell <carolineecrandell@gmail.com>
2021-03-07 02:06:43 -0800

    Audio page added

---
+++ backend/app/public/js/addaudio.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+    // localStorage.clear()
+    // let data = ["asteroblastusstellatus.png", "porpitaprunella.png", "poliopogonamadou.png", "botryllusschlosseri.png", "cassiopeaandromeda.png", "hemipholiscordifera.png", "centrostephanuslongispinus.png", "nautiluspompilius.png", "haeckelianaporcellana.png", "cannorrhizaconnexa.png", "nausithoechallengeri.png", "solmaris.png"]
+    // for (let i = 0; i < data.length; i++) {
+    //   localStorage.setItem(data[i], "/images/" + data[i]);
+    // }
+    $.get("/images/all", function (data) {
+        if (data.length !== 0) {
+            let radios = $("<div id='radios'>")
+            for (let i = 0; i < data.length; i++) {
+                radios.append("<span><input type='radio' id='" + data[i].id + "' name='image-answer' value='" + data[i].id + "' required>")
+                radios.append("<span><label for='" + data[i].id + "'>" + data[i].name + "</label>");
+                radios.append("<span><img class='tableimg' src='" + localStorage.getItem(data[i].png) + "'</img>");
+            }
+            radios.append("</div>")
+            $("#image-area").prepend(radios)
+        }
+    });
+
+    document.querySelector("#audiotag").addEventListener("change", function () {
+        const reader = new FileReader();
+        document.getElementById('form').addEventListener("submit", () => {
+            localStorage.setItem(this.files[0].name, reader.result);
+        })
+        reader.readAsDataURL(this.files[0]);
+    })
+    $.get("/images/all", function (data) {
+        if (data.length !== 0) {
+            let table = $("<table><tr><th>ID</th><th>Name</th><th>Narration</th><th>Image</th><th>Audio</th><th>Actions</th></tr>");
+            for (let i = 0; i < data.length; i++) {
+                let row = $("<tr>");
+                row.append("<td>" + data[i].id + "</td>");
+                row.append("<td>" + data[i].name + "</td>");
+                row.append("<td>" + data[i].text + "</td>");
+                row.append("<td><img class='tableimg' src='" + localStorage.getItem(data[i].png) + "'</img></td>");
+                row.append("<td>" + data[i].default_mp3 + "</td>");
+                row.append("<td><button class='delete' value='" + data[i].id + "' name='" + data[i].png + "'>Delete Audio</button></td>");
+                row.append("</tr>");
+                table.append(row);
+            }
+            table.append("</table>")
+            $("#images-area2").prepend(table)
+        }
+    });
+
+    // $(document).on('click', '.delete', function () {
+    //   localStorage.removeItem(this.name);
+    //   $.ajax({
+    //     url: "/images/delete/" + this.value,
+    //     type: 'DELETE',
+    //     success: function (result) {
+    //       $("#image-area").empty()
+    //       $.get("/images/all", function (data) {
+    //         if (data.length !== 0) {
+    //           let table = $("<table><tr><th>ID</th><th>Name</th><th>Narration</th><th>Image</th><th>Audio</th><th>Actions</th></tr>");
+    //           for (let i = 0; i < data.length; i++) {
+    //             let row = $("<tr>");
+    //             row.append("<td>" + data[i].id + "</td>");
+    //             row.append("<td>" + data[i].name + "</td>");
+    //             row.append("<td>" + data[i].text + "</td>");
+    //             row.append("<td><img class='tableimg' src='" + localStorage.getItem(data[i].png) + "'</img></td>");
+    //             row.append("<td>" + data[i].default_mp3 + "</td>");
+    //             row.append("<td><button class='delete' value='" + data[i].id + "'>Delete</button></td>");
+    //             row.append("</tr>");
+    //             table.append(row);
+    //           }
+    //           table.append("</table>")
+    //           $("#image-area").prepend(table)
+    //         }
+    //       })
+    //     }
+    //   })
+    // })
+
+    function handleFiles(event) {
+        var files = event.target.files;
+        $("#src").attr("src", URL.createObjectURL(files[0]));
+        document.getElementById("preview").load();
+    }
+
+    document.getElementById("audiotag").addEventListener("change", handleFiles, false);
+    document.getElementById("audiotag").addEventListener("change", function () {
+        document.getElementById("hide-audio").style.display = "block";
+    })
+});
--- backend/app/routes/api-routes.js
+++ backend/app/routes/api-routes.js
@@ -91,4 +91,40 @@ module.exports = function (app) {
       res.end();
     });
   })
+
+  // Audios
+
+  // app.post("/audios/new", function (req, res) {
+  //   console.log("Image Data:");
+  //   console.log(req.body);
+  //   let newImage = {
+  //     type: "default",
+  //     name: req.body.name.trim().toUpperCase(),
+  //     text: req.body.narration.trim().replace(/\.\s+([a-z])[^\.]|^(\s*[a-z])[^\.]/g, s => s.replace(/([a-z])/, s => s.toUpperCase())),
+  //     png: req.body.image,
+  //     default_mp3: ""
+  //   };
+  //   let dbQuery = "INSERT INTO images (type, name, text, png, default_mp3) VALUES (?,?,?,?,?)";
+  //   connection.query(dbQuery, [newImage.type, newImage.name, newImage.text, newImage.png, newImage.default_mp3], function (err, result) {
+  //     if (err) throw err;
+  //     console.log("Image successfully saved!");
+  //     res.redirect('/views/addimage.html');
+  //   });
+  // });
+
+  // app.delete("/images/delete/:id", function (req, res) {
+  //   console.log(req.params);
+  //   let dbQuery = "DELETE FROM images WHERE id = ?";
+  //   connection.query(dbQuery, [req.params.id], function (err, result) {
+  //     if (err) throw err;
+  //     console.log("Image deleted from images!");
+  //     res.end();
+  //   });
+  //   let dbQuery2 = "DELETE FROM slideshows WHERE images_id = ?";
+  //   connection.query(dbQuery2, [req.params.id], function (err, result) {
+  //     if (err) throw err;
+  //     console.log("Image deleted from slideshows!");
+  //     res.end();
+  //   });
+  // })
 };
