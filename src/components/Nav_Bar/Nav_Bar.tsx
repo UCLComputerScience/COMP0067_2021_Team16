e64712041cbcd440ff0b4b9c74cec736d1045501
Giuseppe Baldini <giuseppebaldini@live.com>
2021-03-20 13:33:59 +0000

    Changed Visual Settings icon to images

--- src/components/Nav_Bar/Nav_Bar.tsx
+++ src/components/Nav_Bar/Nav_Bar.tsx
@@ -1,6 +1,6 @@
 import { IonButton, IonGrid, IonRow, IonCol, IonIcon} from '@ionic/react';
 import "./Nav_Bar.css";
-import { mail, musicalNotes, desktop, informationCircle } from 'ionicons/icons';
+import { mail, musicalNotes, images, informationCircle } from 'ionicons/icons';
 
 
 const Nav_Bar_New: React.FC = () => (
@@ -8,7 +8,7 @@ const Nav_Bar_New: React.FC = () => (
     <IonRow>
       <IonCol className="ion-column"><IonButton fill="clear" className="ion-button" routerLink='/SubscribePage'><IonIcon icon={mail} size="large" /></IonButton></IonCol>
       <IonCol className="ion-column"><IonButton fill="clear" routerLink="/AudioSettings"><IonIcon icon={musicalNotes} size="large" /></IonButton></IonCol>
-      <IonCol className="ion-column"><IonButton fill="clear" routerLink='/VideoSettings'><IonIcon icon={desktop} size="large" /></IonButton></IonCol>
+      <IonCol className="ion-column"><IonButton fill="clear" routerLink='/VideoSettings'><IonIcon icon={images} size="large" /></IonButton></IonCol>
       <IonCol className="ion-column"><IonButton fill="clear" routerLink="/SupportPage"><IonIcon icon={informationCircle} size="large"/></IonButton></IonCol>
     </IonRow>
   </IonGrid>
