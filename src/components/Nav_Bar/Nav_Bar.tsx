b6362c7cb1d752298a56bbd27645711360e9f928
Giuseppe Baldini <giuseppebaldini@live.com>
2021-03-22 14:42:14 +0000

    Rearranged Settings in one page

--- src/components/Nav_Bar/Nav_Bar.tsx
+++ src/components/Nav_Bar/Nav_Bar.tsx
@@ -7,8 +7,8 @@ const Nav_Bar_New: React.FC = () => (
   <IonGrid className="ion-grid">
     <IonRow>
       <IonCol className="ion-column"><IonButton fill="clear" className="ion-button" routerLink='/SubscribePage'><IonIcon icon={mail} size="large" /></IonButton></IonCol>
-      <IonCol className="ion-column"><IonButton fill="clear" routerLink="/AudioSettings"><IonIcon icon={musicalNotes} size="large" /></IonButton></IonCol>
-      <IonCol className="ion-column"><IonButton fill="clear" routerLink='/VideoSettings'><IonIcon icon={images} size="large" /></IonButton></IonCol>
+      <IonCol className="ion-column"><IonButton fill="clear" routerLink="/AudioGallery"><IonIcon icon={musicalNotes} size="large" /></IonButton></IonCol>
+      <IonCol className="ion-column"><IonButton fill="clear" routerLink='/ImageGallery'><IonIcon icon={images} size="large" /></IonButton></IonCol>
       <IonCol className="ion-column"><IonButton fill="clear" routerLink="/SupportPage"><IonIcon icon={informationCircle} size="large"/></IonButton></IonCol>
     </IonRow>
   </IonGrid>
