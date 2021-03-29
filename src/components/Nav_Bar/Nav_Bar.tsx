f4c170f1e43f7aa5180c5cc956bcc2cba21dfe0c
Caroline Crandell <carolineecrandell@gmail.com>
2021-03-29 10:18:38 -0700

    Edit button commented out

--- src/components/Nav_Bar/Nav_Bar.tsx
+++ src/components/Nav_Bar/Nav_Bar.tsx
@@ -1,15 +1,28 @@
-import { IonButton, IonGrid, IonRow, IonCol, IonIcon} from '@ionic/react';
+import { IonButton, IonGrid, IonRow, IonCol, IonIcon } from "@ionic/react";
 import "./Nav_Bar.css";
-import { mail, musicalNotes, images, informationCircle } from 'ionicons/icons';
-
+import { mail, musicalNotes, images, informationCircle } from "ionicons/icons";
 
 const Nav_Bar_New: React.FC = () => (
   <IonGrid className="ion-grid">
     <IonRow>
-      <IonCol className="ion-column"><IonButton fill="clear" className="ion-button" routerLink='/SubscribePage'><IonIcon icon={mail} size="large" /></IonButton></IonCol>
-      <IonCol className="ion-column"><IonButton fill="clear" routerLink="/AudioGallery"><IonIcon icon={musicalNotes} size="large" /></IonButton></IonCol>
-      <IonCol className="ion-column"><IonButton fill="clear" routerLink='/ImageGallery'><IonIcon icon={images} size="large" /></IonButton></IonCol>
-      <IonCol className="ion-column"><IonButton fill="clear" routerLink="/SupportPage"><IonIcon icon={informationCircle} size="large"/></IonButton></IonCol>
+      <IonCol className="ion-column">
+        <IonButton
+          fill="clear"
+          className="ion-button"
+          routerLink="/SubscribePage"
+        >
+          <IonIcon icon={mail} size="large" />
+        </IonButton>
+      </IonCol>
+      {
+        // <IonCol className="ion-column"><IonButton fill="clear" routerLink="/AudioGallery"><IonIcon icon={musicalNotes} size="large" /></IonButton></IonCol>
+        // <IonCol className="ion-column"><IonButton fill="clear" routerLink='/ImageGallery'><IonIcon icon={images} size="large" /></IonButton></IonCol>
+      }
+      <IonCol className="ion-column">
+        <IonButton fill="clear" routerLink="/SupportPage">
+          <IonIcon icon={informationCircle} size="large" />
+        </IonButton>
+      </IonCol>
     </IonRow>
   </IonGrid>
 );
--- src/pages/SlideshowListingPage/SlideshowListingPage.tsx
+++ src/pages/SlideshowListingPage/SlideshowListingPage.tsx
@@ -1,40 +1,72 @@
-import { IonCol, IonButton, IonIcon, IonContent, IonFooter, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar,} from '@ionic/react';
-import Nav_Bar from '../../components/Nav_Bar/Nav_Bar';
-import Edit_Button from '../../components/EditButton/EditButton';
-import './SlideshowListingPage.css';
-import Slideshow_Items from '../../components/Slideshow_Items/Slideshow_Items';
-import React, { useState } from 'react';
-import AddNewSlideshow from '../../components/AddNewSlideshow/AddNewSlideshow';
-import {settings} from 'ionicons/icons';
+import {
+  IonCol,
+  IonButton,
+  IonIcon,
+  IonContent,
+  IonFooter,
+  IonGrid,
+  IonHeader,
+  IonPage,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import Nav_Bar from "../../components/Nav_Bar/Nav_Bar";
+import Edit_Button from "../../components/EditButton/EditButton";
+import "./SlideshowListingPage.css";
+import Slideshow_Items from "../../components/Slideshow_Items/Slideshow_Items";
+import React, { useState } from "react";
+import AddNewSlideshow from "../../components/AddNewSlideshow/AddNewSlideshow";
+import { settings } from "ionicons/icons";
 
 const SelectionPage: React.FC = () => {
+  const [editMode, setEditMode] = useState(false);
 
-    const[editMode,setEditMode] = useState(false);
-
-    return(
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonGrid>
-                        <IonRow>
-                            <IonCol className='editbutton'><Edit_Button toggleEdit={setEditMode}/></IonCol>
-                            <IonCol className='title'><IonTitle>{editMode ? 'Edit Slideshows':'Select Slideshow'}</IonTitle></IonCol>
-                            <IonCol className='settingsbutton'><IonButton fill="clear" routerLink='/AudioSettings'><IonIcon icon={settings} size="large" /></IonButton></IonCol>
-                        </IonRow>
-                    </IonGrid>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent fullscreen>
-                <Slideshow_Items Editing={editMode}/>
-            </IonContent>);
-            <IonFooter className='bar-footer'>
-                <IonGrid>
-                    {editMode ? <IonRow className='buttoncontainer'><AddNewSlideshow/></IonRow>:null}
-                    {editMode ? null:<IonRow><Nav_Bar/></IonRow>}
-                </IonGrid>
-            </IonFooter>
-        </IonPage>
-);
-}
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonGrid>
+            <IonRow>
+              <IonCol className="editbutton">
+                {
+                  // <Edit_Button toggleEdit={setEditMode}/>
+                }
+              </IonCol>
+              <IonCol className="title">
+                <IonTitle>
+                  {editMode ? "Edit Slideshows" : "Select Slideshow"}
+                </IonTitle>
+              </IonCol>
+              <IonCol className="settingsbutton">
+                <IonButton fill="clear" routerLink="/AudioSettings">
+                  <IonIcon icon={settings} size="large" />
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <Slideshow_Items Editing={editMode} />
+      </IonContent>
+      );
+      <IonFooter className="bar-footer">
+        <IonGrid>
+          {editMode ? (
+            <IonRow className="buttoncontainer">
+              <AddNewSlideshow />
+            </IonRow>
+          ) : null}
+          {editMode ? null : (
+            <IonRow>
+              <Nav_Bar />
+            </IonRow>
+          )}
+        </IonGrid>
+      </IonFooter>
+    </IonPage>
+  );
+};
 
 export default SelectionPage;
