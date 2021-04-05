7b2bb1a58fd4f48399fb3206eacaf4e1448adfeb
Giuseppe Baldini <giuseppebaldini@live.com>
2021-04-05 15:08:29 +0100

    Finalized pagination of 'About Dr. Iya Whiteley'

--- src/pages/SupportPage/DrIyaWhiteley/DrIyaWhiteley.tsx
+++ src/pages/SupportPage/DrIyaWhiteley/DrIyaWhiteley.tsx
@@ -35,17 +35,20 @@ const DrIyaWhiteley: React.FC = () => {
           </IonHeader>
 
           <IonContent>
+
+          <img src={process.env.PUBLIC_URL + 'assets/images/authors.png'} alt="Dr. Iya Whiteley" />
+
           <br/>
           <h4>
-                Space Psychologist, Director of the Centre of Space Medicine,
+                Space Psychologist, <b>Director of the Centre of Space Medicine</b>,
                 University College London, advising to the UK Space Agency. <br/>She
                 developed training programmes for the astronaut class of Major
                 Tim Peake, while working for European Space Agency at the
-                European Astronaut Centre in Germany. <br/>As a Cognitive Engineer
-                and Clinical Psychologist, she specialises in visual perception
+                European Astronaut Centre in Germany. <br/><br/>As a <b>Cognitive Engineer
+                and Clinical Psychologist</b>, she specialises in visual perception
                 and design displays for highly skilled military pilots and
                 astronauts. These top performers need to absorb information
-                instantly and make effective decisions. <br/>Iya is a public speaker
+                instantly and make effective decisions. <br/><br/>Iya is a public speaker
                 and presenter on radio and TV, for example, in BBC Science TV
                 series with Astronaut Chris Hadfield and Dr Kevin Fong
                 presenter, BBC Astronauts: Do you have what it takes?'. <br/>Iya runs
@@ -54,7 +57,7 @@ const DrIyaWhiteley: React.FC = () => {
 
           <h1>
               <a href="https://cosmicbabybooks.com/about">
-              More information on Dr Whiteley
+              More about Dr Whiteley
               </a>
           </h1>
 
