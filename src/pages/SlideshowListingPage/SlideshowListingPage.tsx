dc04d0c31c747ab2581bce17577a6eb39499e1c2
Caroline Crandell <carolineecrandell@gmail.com>
2021-04-18 16:50:07 -0700

    Social media share

--- src/pages/SlideshowListingPage/SlideshowListingPage.tsx
+++ src/pages/SlideshowListingPage/SlideshowListingPage.tsx
@@ -10,13 +10,23 @@ import {
   IonRow,
   IonTitle,
   IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonFabList,
 } from "@ionic/react";
 import Nav_Bar from "../../components/Nav_Bar/Nav_Bar";
+import {
+  settings,
+  share,
+  logoPinterest,
+  logoFacebook,
+  logoInstagram,
+  logoTwitter,
+} from "ionicons/icons";
 import "./SlideshowListingPage.css";
 import Slideshow_Items from "../../components/Slideshow_Items/Slideshow_Items";
 import React, { useState } from "react";
 import AddNewSlideshow from "../../components/AddNewSlideshow/AddNewSlideshow";
-import { settings } from "ionicons/icons";
 
 const SelectionPage: React.FC = () => {
   const [editMode] = useState(false);
@@ -48,6 +58,32 @@ const SelectionPage: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
         <Slideshow_Items Editing={editMode} />
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton>
+            <IonIcon icon={share} />
+          </IonFabButton>
+          <IonFabList side="top">
+            <IonFabButton>
+              <IonIcon icon={logoPinterest} />
+            </IonFabButton>
+          </IonFabList>
+          <IonFabList side="bottom">
+            <IonFabButton>
+              <IonIcon icon={logoFacebook} />
+            </IonFabButton>
+          </IonFabList>
+          <IonFabList side="start">
+            <IonFabButton>
+              <IonIcon icon={logoInstagram} />
+            </IonFabButton>
+          </IonFabList>
+          <IonFabList side="end">
+            <IonFabButton>
+              <IonIcon icon={logoTwitter} />
+            </IonFabButton>
+          </IonFabList>
+        </IonFab>
+        {/* https://github.com/EddyVerbruggen/SocialSharing-PhoneGap-Plugin#5-web-share-api */}
       </IonContent>
       );
       <IonFooter className="bar-footer">
